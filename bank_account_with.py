df8aade7beaedf249264ed4b1c393eac1cb910e6
learner112 <mesiddharthadatta@gmail.com>
2024-04-21 18:33:23 +0530

    class example
    added class example

---
+++ bank_account_with.py
@@ -0,0 +1,26 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.balance = initial_balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return f"Deposit successful. New balance is {self.balance}"
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Insufficient funds"
+        self.balance -= amount
+        return f"Withdrawal successful. New balance is {self.balance}"
+
+    def display_balance(self):
+        return f"Current balance is {self.balance}"
+
+# if __name__ == "__main__":
+#     # Creating an object with an initial balance of 1000
+#     account = BankAccount(1000)
+
+#     # Making transactions and displaying balance
+#     print(account.deposit(500))        # Output: Deposit successful. New balance is 1500
+#     print(account.withdraw(2000))      # Output: Insufficient funds
+#     print(account.withdraw(500))       # Output: Withdrawal successful. New balance is 1000
+#     print(account.display_balance())   # Output: Current balance is 1000
