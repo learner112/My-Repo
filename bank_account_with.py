c691125457512866adf0598d5d44ed3f7dc5a62c
learner112 <mesiddharthadatta@gmail.com>
2025-03-13 17:00:56 +0530

    "added text to bank_account_with.py"

--- bank_account_with.py
+++ bank_account_with.py
@@ -15,6 +15,7 @@ class BankAccount:
     def display_balance(self):
         return f"Current balance is {self.balance}"
 
+"test added"
 # if __name__ == "__main__":
 #     # Creating an object with an initial balance of 1000
 #     account = BankAccount(1000)
